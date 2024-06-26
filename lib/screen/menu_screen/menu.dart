effd8f897deacf660eeda64e1f78e51e58f186d0
MOHAMED7ASSA6 <mh5093351@gmail.com>
2024-06-27 02:07:00 +0300

    ChatScreen

--- lib/screen/menu_screen/menu.dart
+++ lib/screen/menu_screen/menu.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_generative_ai/google_generative_ai.dart';
 import 'package:school/main.dart';
 import 'package:school/screen/profile/profile_screen.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -92,6 +93,17 @@ class MenuScreen extends StatelessWidget {
               },
             ),
             ListTile(
+              title: const Text("chat AI",
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.w600)),
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const ChatScreen()));
+              },
+            ),
+            ListTile(
               title: const Text("Settings",
                   style: TextStyle(
                       color: Colors.white, fontWeight: FontWeight.w600)),
@@ -150,5 +162,156 @@ class MenuScreen extends StatelessWidget {
     } else {
       throw 'Could not launch$url';
     }
+  }
+}
+
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  TextEditingController _userInput = TextEditingController();
+
+  static const apiKey = "AIzaSyDehe3sR8N5SsA5Whe1W5Z_rrfohm-GpcM";
+
+  final model = GenerativeModel(model: 'gemini-pro', apiKey: apiKey);
+
+  final List<Message> _messages = [];
+
+  Future<void> sendMessage() async {
+    final message = _userInput.text;
+
+    setState(() {
+      _messages
+          .add(Message(isUser: true, message: message, date: DateTime.now()));
+    });
+
+    final content = [Content.text(message)];
+    final response = await model.generateContent(content);
+
+    setState(() {
+      _messages.add(Message(
+          isUser: false, message: response.text ?? "", date: DateTime.now()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                colorFilter: new ColorFilter.mode(
+                    Colors.black.withOpacity(0.8), BlendMode.dstATop),
+                image: NetworkImage(
+                    'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEigDbiBM6I5Fx1Jbz-hj_mqL_KtAPlv9UsQwpthZIfFLjL-hvCmst09I-RbQsbVt5Z0QzYI_Xj1l8vkS8JrP6eUlgK89GJzbb_P-BwLhVP13PalBm8ga1hbW5pVx8bswNWCjqZj2XxTFvwQ__u4ytDKvfFi5I2W9MDtH3wFXxww19EVYkN8IzIDJLh_aw/s1920/space-soldier-ai-wallpaper-4k.webp'),
+                fit: BoxFit.cover)),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Expanded(
+                child: ListView.builder(
+                    itemCount: _messages.length,
+                    itemBuilder: (context, index) {
+                      final message = _messages[index];
+                      return Messages(
+                          isUser: message.isUser,
+                          message: message.message,
+                          date: "2:2");
+                    })),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 15,
+                    child: TextFormField(
+                      style: TextStyle(color: Colors.white),
+                      controller: _userInput,
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          label: Text('Enter Your Message')),
+                    ),
+                  ),
+                  Spacer(),
+                  IconButton(
+                      padding: EdgeInsets.all(12),
+                      iconSize: 30,
+                      style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.black),
+                          foregroundColor:
+                              MaterialStateProperty.all(Colors.white),
+                          shape: MaterialStateProperty.all(CircleBorder())),
+                      onPressed: () {
+                        sendMessage();
+                      },
+                      icon: Icon(Icons.send))
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Message {
+  final bool isUser;
+  final String message;
+  final DateTime date;
+
+  Message({required this.isUser, required this.message, required this.date});
+}
+
+class Messages extends StatelessWidget {
+  final bool isUser;
+  final String message;
+  final String date;
+
+  const Messages(
+      {super.key,
+      required this.isUser,
+      required this.message,
+      required this.date});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      padding: EdgeInsets.all(15),
+      margin: EdgeInsets.symmetric(vertical: 15)
+          .copyWith(left: isUser ? 100 : 10, right: isUser ? 10 : 100),
+      decoration: BoxDecoration(
+          color: isUser ? Colors.blueAccent : Colors.grey.shade400,
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(10),
+              bottomLeft: isUser ? Radius.circular(10) : Radius.zero,
+              topRight: Radius.circular(10),
+              bottomRight: isUser ? Radius.zero : Radius.circular(10))),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            message,
+            style: TextStyle(
+                fontSize: 16, color: isUser ? Colors.white : Colors.black),
+          ),
+          Text(
+            date,
+            style: TextStyle(
+              fontSize: 10,
+              color: isUser ? Colors.white : Colors.black,
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
